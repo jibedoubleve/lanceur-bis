be1a5a3b73d4248cd6ee8a284b78e0e8c3cf7003
jibedoubleve <jibedoubleve@gmail.com>
2022-07-06 14:45:04 +0200

    Fix #3 - Double special char

--- src/Lanceur.Tests/BusinessLogic/CommandlineShould.cs
+++ src/Lanceur.Tests/BusinessLogic/CommandlineShould.cs
@@ -1,5 +1,4 @@
 ﻿using FluentAssertions;
-using Lanceur.Infra;
 using Lanceur.Infra.Managers;
 using Xunit;
 
@@ -7,38 +6,7 @@ namespace Lanceur.Tests.BusinessLogic
 {
     public class CommandlineShould
     {
-        [Fact]
-        public void ReturnsEmptyOnEmptyCmdline()
-        {
-            var processor = new CmdlineManager();
-            var line = processor.BuildFromText(string.Empty);
-
-            line.Name.Should().BeEmpty();
-            line.Parameters.Should().BeEmpty();
-        }
-        [Fact]
-        public void ReturnsEmptyOnNullCmdline()
-        {
-            var processor = new CmdlineManager();
-            var line = processor.BuildFromText(null);
-
-            line.Name.Should().BeEmpty();
-            line.Parameters.Should().BeEmpty();
-        }
-
-        [Theory]
-        [InlineData("cls", "cls arg1 arg2")]
-        [InlineData("excel", "excel arg1 arg2")]
-        [InlineData("excel", "excel")]
-        [InlineData("%", "% arg1 arg2")]
-        [InlineData("$", "$arg1 arg2")]
-        public void HaveName(string asExpected, string actual)
-        {
-            var processor = new CmdlineManager();
-            var line = processor.BuildFromText(actual);
-
-            line.Name.Should().Be(asExpected);
-        }
+        #region Methods
 
         [Theory]
         [InlineData("arg1 arg2", "cls arg1 arg2")]
@@ -56,5 +24,62 @@ namespace Lanceur.Tests.BusinessLogic
 
             line.Parameters.Should().Be(asExpected);
         }
+
+        [Theory]
+        [InlineData("cls", "cls arg1 arg2")]
+        [InlineData("excel", "excel arg1 arg2")]
+        [InlineData("excel", "excel")]
+        [InlineData("%", "% arg1 arg2")]
+        [InlineData("$", "$arg1 arg2")]
+        [InlineData("a", "a")]
+        public void HaveName(string asExpected, string actual)
+        {
+            var processor = new CmdlineManager();
+            var line = processor.BuildFromText(actual);
+
+            line.Name.Should().Be(asExpected);
+        }
+
+        [Theory]
+        [InlineData("? hello world", "?")]
+        [InlineData("??", "??")]
+        [InlineData("&& un deux trois", "&&")]
+        [InlineData("&", "&")]
+        [InlineData("", "")]
+        [InlineData("m&", "m&")]
+        [InlineData("m&&", "m&&")]
+        [InlineData("m& fff", "m&")]
+        [InlineData("m&& fff", "m&&")]
+        public void RecogniseDoubleOrSingleSpecialChar(string cmdline, string expected)
+        {
+            var processor = new CmdlineManager();
+
+            processor
+                .BuildFromText(cmdline)
+                .Name
+                .Should().Be(expected);
+        }
+
+        [Fact]
+        public void ReturnsEmptyOnEmptyCmdline()
+        {
+            var processor = new CmdlineManager();
+            var line = processor.BuildFromText(string.Empty);
+
+            line.Name.Should().BeEmpty();
+            line.Parameters.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void ReturnsEmptyOnNullCmdline()
+        {
+            var processor = new CmdlineManager();
+            var line = processor.BuildFromText(null);
+
+            line.Name.Should().BeEmpty();
+            line.Parameters.Should().BeEmpty();
+        }
+
+        #endregion Methods
     }
 }
