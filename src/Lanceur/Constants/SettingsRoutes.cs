1a5cbbac8a85115a63b7fc24d619e8118d280eb1
jibedoubleve <jibedoubleve@gmail.com>
2023-01-06 13:55:58 +0100

    Issue 61: Settings, shows keywords by default

---
+++ src/Lanceur/Constants/SettingsRoutes.cs
@@ -0,0 +1,19 @@
+﻿namespace Lanceur.Constants
+{
+    internal struct SettingsRoutes
+    {
+        #region Fields
+
+        public const string Doubloons = "doubloonsview";
+        public const string History = "historyview";
+        public const string InvalidAliases = "invalidaliasview";
+        public const string Keywords = "keywordsview";
+        public const string MostUsed = "mostusedview";
+        public const string Plugin = "pluginsview";
+        public const string Sessions = "sessionsview";
+        public const string Settings = "settings";
+        public const string Trends = "trendsview";
+
+        #endregion Fields
+    }
+}
