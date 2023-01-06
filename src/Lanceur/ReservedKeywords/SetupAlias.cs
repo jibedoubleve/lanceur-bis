b195a5b7bf3ce0453b2d88ee1d6547319100d5d3
jibedoubleve <jibedoubleve@users.noreply.github.com>
2023-01-06 22:40:25 +0100

    Issue 61: Settings, shows keywords by default (#72)

--- src/Lanceur/ReservedKeywords/SetupAlias.cs
+++ src/Lanceur/ReservedKeywords/SetupAlias.cs
@@ -1,6 +1,8 @@
-﻿using Lanceur.Core;
+﻿using Lanceur.Constants;
+using Lanceur.Core;
 using Lanceur.Core.Models;
 using Lanceur.Views;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Threading.Tasks;
@@ -15,6 +17,7 @@ namespace Lanceur.ReservedKeywords
         public override Task<IEnumerable<QueryResult>> ExecuteAsync(Cmdline cmdline = null)
         {
             var view = new SettingsView();
+            view.ViewModel.PushNavigation.Execute(SettingsRoutes.Keywords).Subscribe();
             view.Show();
             return NoResultAsync;
         }
