822e23dcfe9656bef4c922f7cf71b4e9edbb26bb
jibedoubleve <jibedoubleve@gmail.com>
2023-04-24 18:26:55 +0200

    (#158) Fix debug macro

---
+++ src/Lanceur.Tests/Utils/DebugMacroExecutor.cs
@@ -0,0 +1,30 @@
+﻿using Lanceur.Core.Managers;
+using Lanceur.Core.Models;
+using Lanceur.Macros;
+using Xunit;
+
+namespace Lanceur.Tests.Utils
+{
+    internal class DebugMacroExecutor : IExecutionManager
+    {
+        #region Methods
+
+        public async Task<ExecutionResponse> ExecuteAsync(ExecutionRequest request)
+        {
+            if (request.QueryResult is DebugMacro macro)
+            {
+                return new()
+                {
+                    Results = await macro.ExecuteAsync(new Cmdline("debug")),
+                };
+            }
+            else
+            {
+                Assert.True(false, "Request not containing a 'DebugMAcro'");
+                return null;
+            }
+        }
+
+        #endregion Methods
+    }
+}
