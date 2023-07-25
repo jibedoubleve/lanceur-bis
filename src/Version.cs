86d7583f0aa02ff28e76084c7694a8ff8bc8ea29
jibedoubleve <jibedoubleve@gmail.com>
2023-07-25 18:53:28 +0200

    (#278) Remove src/Version.cs

--- src/Version.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyVersion("0.8.1.17")]
-[assembly: AssemblyFileVersion("0.8.1.0")]
-[assembly: AssemblyInformationalVersion("0.8.1-beta.17+Branch.develop.Sha.f4f01f3e72553f4cd9334d6e7f386cf5d18cdc38")]
-
