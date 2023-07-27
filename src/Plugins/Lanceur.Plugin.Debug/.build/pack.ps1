e32e48c6db3e03af25e542b8552656a9a986686f
jibedoubleve <jibedoubleve@gmail.com>
2023-07-27 10:32:13 +0200

    (#287) Add script to build plugin

---
+++ src/Plugins/Lanceur.Plugin.Debug/.build/pack.ps1
@@ -0,0 +1,29 @@
+﻿param (
+    [parameter(mandatory=$true)]$name,
+    [parameter(mandatory=$true)]$version,
+    [parameter(mandatory=$true)]$output
+)
+
+
+Push-Location
+Write-Host "=== Pack plugin ===" -ForeGroundColor Yellow
+
+Write-Host "`t=> Go to output directory"
+Set-Location $output
+
+Write-Host "`t=> Clean useless files"
+Remove-Item *.deps.json
+Remove-Item *.pdb
+Remove-Item *Lanceur.Plugin.Debug.dll
+Remove-Item *.runtimeconfig.json
+Remove-Item *.lpk
+
+Write-Host "`t=> Zip plugin"
+$fileName = $name + "_" + $version
+$zip      = Join-Path -Path $output -ChildPath $fileName
+Compress-Archive -Path *.* -DestinationPath "$zip.zip"
+
+Write-Host "`t=> Change extension from '.zip' to '.lpk'"
+Rename-Item "$fileName.zip" -NewName "$fileName`.lpk"
+
+Pop-Location
