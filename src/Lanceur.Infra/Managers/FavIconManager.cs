7986616d65df526c4eace7c3bff2f2821977e9ae
jibedoubleve <jibedoubleve@gmail.com>
2024-07-30 15:05:11 +0200

    (#521) Improve FavIcon download facility

--- src/Lanceur.Infra/Managers/FavIconManager.cs
+++ src/Lanceur.Infra/Managers/FavIconManager.cs
@@ -5,6 +5,8 @@ using Lanceur.SharedKernel.Web;
 using Microsoft.Extensions.Logging;
 using System.Text.RegularExpressions;
 using Lanceur.SharedKernel.Mixins;
+using Splat;
+using ILogger = Microsoft.Extensions.Logging.ILogger;
 
 namespace Lanceur.Infra.Managers
 {
@@ -19,38 +21,50 @@ namespace Lanceur.Infra.Managers
         private static readonly Regex IsMacroRegex = new("@.*@");
 
         private readonly IFavIconDownloader _favIconDownloader;
-        private readonly ILogger<IFavIconManager> _logger;
+        private readonly ILogger _logger;
+        private readonly string _imageRepository;
 
         #endregion Fields
 
         #region Constructors
 
-        public FavIconManager(IPackagedAppSearchService searchService, IFavIconDownloader favIconDownloader, ILoggerFactory appLoggerFactory)
+        /// <param name="imageRepository">
+        /// This is used for unit tests. Keep default value unless you're testing
+        /// </param>
+        public FavIconManager(
+            IPackagedAppSearchService searchService, 
+            IFavIconDownloader favIconDownloader, 
+            ILoggerFactory loggerFactory,
+            string imageRepository = null)
         {
+            _imageRepository = imageRepository ?? Paths.ImageRepository; 
             ArgumentNullException.ThrowIfNull(searchService);
             ArgumentNullException.ThrowIfNull(favIconDownloader);
-            ArgumentNullException.ThrowIfNull(appLoggerFactory);
+            ArgumentNullException.ThrowIfNull(loggerFactory);
 
             _favIconDownloader = favIconDownloader;
-            _logger = appLoggerFactory.CreateLogger<FavIconManager>();
+            _logger = loggerFactory.CreateLogger<FavIconDownloader>();
         }
 
         #endregion Constructors
 
         #region Methods
 
-        public async Task RetrieveFaviconAsync(string fileName)
+        public async Task RetrieveFaviconAsync(string url)
         {
-            if (fileName is null) return;
-            if (IsMacroRegex.Match(fileName).Success) return;
-            if (!Uri.TryCreate(fileName, UriKind.Absolute, out var uri)) return;
+            if (url is null) return;
+            if (IsMacroRegex.Match(url).Success) return;
+            if (!Uri.TryCreate(url, UriKind.Absolute, out var uri)) return;
             
-            var output = Path.Combine(Paths.ImageRepository, $"{Paths.FaviconPrefix}{uri.Host}.png");
+            var output = Path.Combine(_imageRepository, $"{FavIconHelpers.FilePrefix}{uri.Host}.png");
             if (File.Exists(output)) return;
-            
-            if (!await _favIconDownloader.CheckExistsAsync(new($"{uri.Scheme}://{uri.Host}"))) return;
 
-            await _favIconDownloader.SaveToFileAsync(uri, output);
+            var uriAuthority = uri.GetAuthority(); 
+            _logger.LogInformation("Getting favicon for {Uri}", uriAuthority);
+            
+            if (!await _favIconDownloader.CheckExistsAsync(uriAuthority)) return;
+
+            await _favIconDownloader.SaveToFileAsync(uriAuthority, output);
         }
 
         #endregion Methods
---
+++ src/Tests/Lanceur.Tests/Functional/FavIconManagerShould.cs
@@ -0,0 +1,55 @@
+using FluentAssertions;
+using Lanceur.Core.Services;
+using Lanceur.Infra.Managers;
+using Lanceur.SharedKernel.Mixins;
+using Lanceur.SharedKernel.Web;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using Xunit;
+
+namespace Lanceur.Tests.Functional;
+
+public class FavIconManagerShould
+{
+    [Theory]
+    [InlineData("http://www.google.com", "http://www.google.com")]
+    [InlineData("http://www.google.com:4001", "http://www.google.com:4001")]
+    [InlineData("http://www.google.com:80", "http://www.google.com:80")]
+    [InlineData("https://www.google.com", "https://www.google.com")]
+    [InlineData("https://www.google.com:4001", "https://www.google.com:4001")]
+    [InlineData("https://www.google.com:80", "https://www.google.com:80")]
+    [InlineData("https://www.google.com/some/index.html", "https://www.google.com")]
+    [InlineData("https://www.google.com:4001/some/index.html", "https://www.google.com:4001")]
+    [InlineData("https://www.google.com:80/some/index.html", "https://www.google.com:80")]
+    public async Task RetrieveExpectedUrl(string url, string asExpected)
+    {
+        // ARRANGE
+        var searchService = Substitute.For<IPackagedAppSearchService>();
+        var favIconDownloader = Substitute.For<IFavIconDownloader>();
+        var logger = Substitute.For<ILoggerFactory>();
+        var repository = Path.GetTempPath();
+        
+        // ACT
+        var manager = new FavIconManager(searchService, favIconDownloader, logger, repository);
+        await manager.RetrieveFaviconAsync(url);
+        
+        // ASSERT
+        await favIconDownloader.Received()
+                               .CheckExistsAsync(new(asExpected));
+
+    }
+
+    [Theory]
+    [InlineData("http://www.google.com", "http://www.google.com/favicon.ico")]
+    [InlineData("http://www.google.com:4001", "http://www.google.com:4001/favicon.ico")]
+    [InlineData("http://www.google.com:80", "http://www.google.com:80/favicon.ico")]
+    [InlineData("https://www.google.com", "https://www.google.com/favicon.ico")]
+    [InlineData("https://www.google.com:4001", "https://www.google.com:4001/favicon.ico")]
+    [InlineData("https://www.google.com:80", "https://www.google.com:80/favicon.ico")]
+    public void CreateFaviconUri(string url, string expected)
+    {
+        var thisUri = new Uri(expected);
+        new Uri(url).GetFavicons()
+                    .Should().Contain(thisUri);
+    }
+}
