de7a625d1a751d3c429197ea97e82d14d173f39c
jibedoubleve <jibedoubleve@gmail.com>
2024-07-30 17:05:14 +0200

    (#520) Center window on monitor when it goes out of bounds

--- src/Lanceur.Infra.Win32/Utils/ScreenRuler.cs
+++ src/Lanceur.Infra.Win32/Utils/ScreenRuler.cs
@@ -20,13 +20,13 @@ namespace Lanceur.Infra.Win32.Utils
 
         #region Methods
 
-        public static Coordinate GetCenterCoordinate(double topOffset = double.MaxValue)
+        private static Coordinate GetCenterCoordinate(double topOffset)
         {
             var screenWidth = SystemParameters.PrimaryScreenWidth;
             var screenHeight = SystemParameters.PrimaryScreenHeight;
 
             var x = (screenWidth - WindowWidth) / 2;
-            var y = topOffset == double.MaxValue
+            var y = topOffset >= double.MaxValue
                 ? (screenHeight - WindowHeight) / 2
                 : topOffset;
 
@@ -43,11 +43,32 @@ namespace Lanceur.Infra.Win32.Utils
         public static void SetWindowPosition(Coordinate coordinate)
         {
             var win = Application.Current.MainWindow;
-
             if (win is null) return;
 
             win.Left = coordinate.X;
             win.Top = coordinate.Y;
+        }
+        
+        
+        public static bool IsWindowOutOfScreen()
+        {
+            var win = Application.Current.MainWindow;
+            if (win is null) return true;
+            
+            // Obtain the dimensions of the primary screen
+            var workArea = SystemParameters.WorkArea;
+
+            // Obtain the dimensions and position of the window
+            var winLeft = win.Left;
+            var winTop = win.Top;
+            var winWidth = winLeft + win.Width;
+            var winHeight = winTop + win.Height;
+
+            // Check if the window is completely or partially off the screen
+            return winLeft < workArea.Left ||
+                   winTop < workArea.Top ||
+                   winWidth > workArea.Right ||
+                   winHeight > workArea.Bottom;
         }
 
         #endregion Methods
