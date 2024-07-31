51c05cec4c7d20daa72b37e09160601a58a439b5
jibedoubleve <jibedoubleve@gmail.com>
2024-07-31 14:41:54 +0200

    (#533) Update version MessageBox

--- src/Lanceur/ReservedKeywords/VersionAlias.cs
+++ src/Lanceur/ReservedKeywords/VersionAlias.cs
@@ -24,15 +24,12 @@ namespace Lanceur.ReservedKeywords
         public override Task<IEnumerable<QueryResult>> ExecuteAsync(Cmdline cmdline = null)
         {
             var asm = Assembly.GetExecutingAssembly();
-            var version = asm.GetName().Version.ToString();
-            var fileVersion = FileVersionInfo.GetVersionInfo(asm.Location).FileVersion.ToString();
-
-            var semver = FileVersionInfo.GetVersionInfo(asm.Location).ProductVersion.ToString();
-            var semverSplit = semver.Split(new string[] { "+" }, StringSplitOptions.RemoveEmptyEntries);
-            semver = semverSplit.Length > 0 ? semverSplit[0] : semver;
+            var semver = FileVersionInfo.GetVersionInfo(asm.Location).ProductVersion;
+            var semverSplit = semver?.Split(new string[] { "+" }, StringSplitOptions.RemoveEmptyEntries);
+            semver = semverSplit?.Length > 0 ? semverSplit[0] : semver;
 
             var nl = Environment.NewLine;
-            var msg = $"Version: {version}{nl}File Version: {fileVersion}{nl}SemVer: {semver}{nl}Author: JB Wautier";
+            var msg = $"Lanceur {semver}{nl}{nl}Written by Jean-Baptiste Wautier";
             MessageBox.Show(msg, "Lanceur", MessageBoxButton.OK, MessageBoxImage.Information);
 
             return NoResultAsync;
