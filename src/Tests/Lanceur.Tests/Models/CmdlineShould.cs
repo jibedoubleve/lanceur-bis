801b8f13eeaa2fd0d36ccfab6727a59d320deec3
jibedoubleve <jibedoubleve@gmail.com>
2024-08-02 17:10:15 +0200

    (#554) Add '.' as a one char command

--- src/Tests/Lanceur.Tests/Models/CmdlineShould.cs
+++ src/Tests/Lanceur.Tests/Models/CmdlineShould.cs
@@ -1,37 +1,60 @@
 ﻿using FluentAssertions;
 using Lanceur.Core.Models;
+using Lanceur.Infra.Managers;
 using Xunit;
 
-namespace Lanceur.Tests.Models
+namespace Lanceur.Tests.Models;
+
+public class CmdlineShould
 {
-    public class CmdlineShould
+    #region Methods
+
+    [Fact]
+    public void HaveEmptyNameByDefault() { Cmdline.Empty.Name.Should().BeEmpty(); }
+
+    [Fact]
+    public void HaveEmptyNameWhenCtorNull() { new Cmdline(null, null).Name.Should().BeEmpty(); }
+
+    [Fact]
+    public void HaveEmptyParametersByDefault() { Cmdline.Empty.Parameters.Should().BeEmpty(); }
+
+    [Fact]
+    public void HaveEmptyParametersWhenCtorNull() { new Cmdline(null, null).Parameters.Should().BeEmpty(); }
+
+    [Theory,
+     InlineData("$AAAA aa"),
+     InlineData("&AAAA aa"),
+     InlineData("|AAAA aa"),
+     InlineData("@AAAA aa"),
+     InlineData("#AAAA aa"),
+     InlineData("(AAAA aa"),
+     InlineData(")AAAA aa"),
+     InlineData("§AAAA aa"),
+     InlineData("!AAAA aa"),
+     InlineData("{AAAA aa"),
+     InlineData("}AAAA aa"),
+     InlineData("-AAAA aa"),
+     InlineData("_AAAA aa"),
+     InlineData("\\AAAA aa"),
+     InlineData("+AAAA aa"),
+     InlineData("*AAAA aa"),
+     InlineData("/AAAA aa"),
+     InlineData("=AAAA aa"),
+     InlineData("<AAAA aa"),
+     InlineData(">AAAA aa"),
+     InlineData(",AAAA aa"),
+     InlineData(";AAAA aa"),
+     InlineData(":AAAA aa"),
+     InlineData("%AAAA aa"),
+     InlineData("?AAAA aa"),
+     InlineData(".AAAA aa")]
+    public void HandleSpecialCmdCharacter(string cmdline)
     {
-        #region Methods
-
-        [Fact]
-        public void HaveEmptyNameByDefault()
-        {
-            Cmdline.Empty.Name.Should().BeEmpty();
-        }
-
-        [Fact]
-        public void HaveEmptyNameWhenCtorNull()
-        {
-            new Cmdline(null, null).Name.Should().BeEmpty();
-        }
-
-        [Fact]
-        public void HaveEmptyParametersByDefault()
-        {
-            Cmdline.Empty.Parameters.Should().BeEmpty();
-        }
-
-        [Fact]
-        public void HaveEmptyParametersWhenCtorNull()
-        {
-            new Cmdline(null, null).Parameters.Should().BeEmpty();
-        }
-
-        #endregion Methods
+        var builder = new CmdlineManager();
+        builder.BuildFromText(cmdline)
+               .Name.Should()
+               .Be(cmdline[0].ToString());
     }
+
+    #endregion Methods
 }
