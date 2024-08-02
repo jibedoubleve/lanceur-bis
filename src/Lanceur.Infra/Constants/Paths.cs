e48a324cbb7fbc20adbcae4b0a31df2565abf80d
jibedoubleve <jibedoubleve@gmail.com>
2024-08-02 22:56:27 +0200

    (#565) Rename LogUrl

--- src/Lanceur.Infra/Constants/Paths.cs
+++ src/Lanceur.Infra/Constants/Paths.cs
@@ -7,7 +7,7 @@ public static class Paths
 {
     #region Fields
 
-    private static readonly Conditional<string> LogUrlValue = new("http://localhost:5341", "http://ec2-35-180-121-110.eu-west-3.compute.amazonaws.com:5341");
+    private static readonly Conditional<string> TelemetryUrlValue = new("http://localhost:5341", "http://ec2-35-180-121-110.eu-west-3.compute.amazonaws.com:5341");
 
     #endregion Fields
 
@@ -16,7 +16,7 @@ public static class Paths
     public static string DefaultDb { get; } = @"%appdata%\probel\lanceur2\data.sqlite".ExpandPath();
     public static string ImageRepository { get; } = @"%appdata%\probel\lanceur2\thumbnails".ExpandPath();
     public static string LogFile { get; } = @"%appdata%\probel\lanceur2\logs\probel-lanceur..clef".ExpandPath();
-    public static string LogUrl => LogUrlValue;
+    public static string TelemetryUrl => TelemetryUrlValue;
     public static string PluginUninstallLogs { get; } = @"%appdata%\probel\lanceur2\.plugin-uninstall".ExpandPath();
     public static string Settings { get; } = @"%appdata%\probel\lanceur2\settings.json".ExpandPath();
 
