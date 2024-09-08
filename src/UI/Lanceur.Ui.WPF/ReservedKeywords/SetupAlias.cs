aff872abc9f1d4b4a66e433d26e887db0a1a9b0e
jibedoubleve <jibedoubleve@gmail.com>
2024-09-08 17:39:07 +0200

    (#317) Fix icons

---
+++ src/UI/Lanceur.Ui.WPF/ReservedKeywords/SetupAlias.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using Lanceur.Core;
+using Lanceur.Core.Models;
+
+namespace Lanceur.ReservedKeywords;
+
+[ReservedAlias("setup")]
+[Description("Open the setup page")]
+public class SetupAlias : SelfExecutableQueryResult
+{
+    #region Constructors
+
+    public SetupAlias(IServiceProvider serviceProvider) { }
+
+    #endregion
+
+    #region Properties
+
+    public override string Icon => "Settings24";
+
+    #endregion
+
+    #region Methods
+
+    public override Task<IEnumerable<QueryResult>> ExecuteAsync(Cmdline cmdline = null) => NoResultAsync;
+
+    #endregion
+}
