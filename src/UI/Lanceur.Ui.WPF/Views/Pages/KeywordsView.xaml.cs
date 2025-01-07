0aaafd854c81d6e3512a379d8fa9ee850d6bc5f5
jibedoubleve <jibedoubleve@gmail.com>
2025-01-07 20:13:54 +0100

    (maint) Fix MessageBox registration

--- src/UI/Lanceur.Ui.WPF/Views/Pages/KeywordsView.xaml.cs
+++ src/UI/Lanceur.Ui.WPF/Views/Pages/KeywordsView.xaml.cs
@@ -1,18 +1,22 @@
 using System.Windows;
 using CommunityToolkit.Mvvm.Messaging;
+using Lanceur.SharedKernel.DI;
 using Lanceur.Ui.Core.Messages;
 using Lanceur.Ui.Core.ViewModels.Pages;
+using Microsoft.Extensions.Logging;
 using Wpf.Ui;
 using Wpf.Ui.Controls;
 using Wpf.Ui.Extensions;
 
 namespace Lanceur.Ui.WPF.Views.Pages;
 
+[Singleton]
 public partial class KeywordsView : IDisposable
 {
     #region Fields
 
     private readonly CodeEditorView _codeEditorView;
+    private readonly ILogger<KeywordsView> _logger;
 
     private readonly IContentDialogService _contentDialogService;
 
@@ -23,14 +27,21 @@ public partial class KeywordsView : IDisposable
     public KeywordsView(
         KeywordsViewModel viewModel,
         IContentDialogService contentDialogService,
-        CodeEditorView codeEditorView
+        CodeEditorView codeEditorView,
+        ILogger<KeywordsView> logger
     )
     {
-        WeakReferenceMessenger.Default.Register<KeywordsView, QuestionRequestMessage>(this, (_, m) => m.Reply(HandleMessageBoxAsync(m)));
-        InitializeComponent();
+        
         _contentDialogService = contentDialogService;
         _codeEditorView = codeEditorView;
+        _logger = logger;
+        
         DataContext = ViewModel = viewModel;
+
+        WeakReferenceMessenger.Default.Register<KeywordsView, QuestionRequestMessage>(this, (_, m) => m.Reply(HandleMessageBoxAsync(m)));
+        _logger.LogTrace("Registered {View} with {Massage}", nameof(KeywordsView), nameof(QuestionRequestMessage));
+        
+        InitializeComponent();
     }
 
     #endregion
@@ -61,7 +72,11 @@ public partial class KeywordsView : IDisposable
         WeakReferenceMessenger.Default.Send<NavigationMessage>(new(content));
     }
 
-    public void Dispose() { WeakReferenceMessenger.Default.Unregister<KeywordsView>(this); }
+    public void Dispose()
+    {
+        WeakReferenceMessenger.Default.Unregister<KeywordsView>(this); 
+        _logger.LogTrace("Unregistered {View} with {Massage}", nameof(KeywordsView), nameof(QuestionRequestMessage));
+    }
 
     #endregion
 }
