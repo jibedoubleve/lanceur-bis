3ff8b596ed6cd5b4ae8050b33cbfb434fc614f36
jibedoubleve <jibedoubleve@gmail.com>
2025-02-01 22:50:26 +0100

    (#655) Now Lua scripts are saved

--- src/UI/Lanceur.Ui.Core/ViewModels/Pages/CodeEditorViewModel.cs
+++ src/UI/Lanceur.Ui.Core/ViewModels/Pages/CodeEditorViewModel.cs
@@ -1,8 +1,10 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using Lanceur.Core.Models;
+using Lanceur.SharedKernel.DI;
 
 namespace Lanceur.Ui.Core.ViewModels.Pages;
 
+[Singleton]
 public partial class CodeEditorViewModel : ObservableObject
 {
     #region Fields
