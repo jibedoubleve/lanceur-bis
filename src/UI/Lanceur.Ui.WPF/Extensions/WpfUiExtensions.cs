8b1dbae07308638e23d37237ed4a962a829f2e8a
jibedoubleve <jibedoubleve@gmail.com>
2025-02-04 20:31:39 +0100

    (#684) Fix unsupported backdrop style

--- src/UI/Lanceur.Ui.WPF/Extensions/WpfUiExtensions.cs
+++ src/UI/Lanceur.Ui.WPF/Extensions/WpfUiExtensions.cs
@@ -1,3 +1,4 @@
+using Lanceur.SharedKernel.Utils;
 using Wpf.Ui.Controls;
 
 namespace Lanceur.Ui.WPF.Extensions;
@@ -12,8 +13,12 @@ public static class WpfUiExtensions
         if (string.Equals(value, WindowBackdropType.None.ToString(), StringComparison.CurrentCultureIgnoreCase)) return WindowBackdropType.None;
         if (string.Equals(value, WindowBackdropType.Auto.ToString(), StringComparison.CurrentCultureIgnoreCase)) return WindowBackdropType.Auto;
         if (string.Equals(value, WindowBackdropType.Mica.ToString(), StringComparison.CurrentCultureIgnoreCase)) return WindowBackdropType.Mica;
+        if (string.Equals(value, WindowBackdropType.Tabbed.ToString(), StringComparison.CurrentCultureIgnoreCase)) return WindowBackdropType.Tabbed;
 
-        throw new NotSupportedException($"{value} is not a wpf backdrop type.");
+        return new Conditional<Func<WindowBackdropType>>(OnDebug, OnRelease).Value();
+
+        WindowBackdropType OnRelease() => WindowBackdropType.Auto;
+        WindowBackdropType OnDebug() => throw new NotSupportedException($"{value} is not a wpf backdrop type.");
     }
 
     #endregion
