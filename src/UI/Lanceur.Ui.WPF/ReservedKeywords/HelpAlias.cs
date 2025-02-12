bb8127f887ecdc36c649c0c6af566f7d4bb8968e
jibedoubleve <jibedoubleve@gmail.com>
2025-02-12 21:56:07 +0100

    (#713) Add help keyword

---
+++ src/UI/Lanceur.Ui.WPF/ReservedKeywords/HelpAlias.cs
@@ -0,0 +1,39 @@
+using System.ComponentModel;
+using System.Diagnostics;
+using Lanceur.Core;
+using Lanceur.Core.Models;
+
+namespace Lanceur.Ui.WPF.ReservedKeywords;
+
+[ReservedAlias("help")]
+[Description("Open help page")]
+public class HelpAlias : SelfExecutableQueryResult
+{
+    #region Fields
+
+    private const string Url = "https://jibedoubleve.github.io/lanceur-bis/";
+
+    #endregion
+
+    #region Constructors
+
+    public HelpAlias(IServiceProvider _) { }
+
+    #endregion
+
+    #region Properties
+
+    public override string Icon => "ChatHelp24";
+
+    #endregion
+
+    #region Methods
+
+    public override Task<IEnumerable<QueryResult>> ExecuteAsync(Cmdline? cmdline = null)
+    {
+        Process.Start("explorer.exe", Url);
+        return NoResultAsync;
+    }
+
+    #endregion
+}
