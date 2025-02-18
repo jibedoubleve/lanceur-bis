032ecbc8421627b581bc8ee02d00a732d2c1ee42
jibedoubleve <jibedoubleve@gmail.com>
2025-02-18 15:30:53 +0100

    (#740) Fix store shortcut override

--- src/UI/Lanceur.Ui.WPF/Converters/StoreOrchestrationToStringConverter.cs
+++ src/UI/Lanceur.Ui.WPF/Converters/StoreOrchestrationToStringConverter.cs
@@ -5,22 +5,32 @@ namespace Lanceur.Ui.WPF.Converters;
 
 public class StoreOrchestrationToStringConverter : IValueConverter
 {
+    #region Fields
+
+    /// <remarks>
+    /// The backslash should be the first item in the list otherwise it'll be escaped multiple times...
+    /// </remarks>
+    private readonly string[] _toEscape = ["\\", ".", "*", "+", "?", "{", "}", "[", "]", "(", ")", "`", "^", "$"];
+
+    #endregion
+
     #region Methods
 
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        if (value is string storeOverride)
-            return storeOverride.Replace(@"^\s{0,}", "")
-                                .Replace(".*", "");
+        if (value is not string storeOverride) return Binding.DoNothing;
 
-        return Binding.DoNothing;
+        return storeOverride.Replace(@"^\s{0,}", "")
+                            .Replace(".*", "");
     }
 
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        if (value is string storeOverride) return $@"^\s{{0,}}{storeOverride}.*";
+        if (value is not string storeOverride) return Binding.DoNothing;
+        
+        foreach (var character in _toEscape) storeOverride = storeOverride.Replace(character, $"\\{character}");
 
-        return Binding.DoNothing;
+        return $@"^\s{{0,}}{storeOverride}.*";
     }
 
     #endregion
