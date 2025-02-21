3e5b7121580b55e45117c2266472962370c6478f
jibedoubleve <jibedoubleve@gmail.com>
2025-02-21 18:41:27 +0100

    (maint) Add SQL cleaning script

---
+++ src/Lanceur.Scripts/SQL/script-2.2.2.sql
@@ -0,0 +1,5 @@
+/* Remove invalid records from the alias_argument table.  
+ * Each alias argument must be associated with a valid alias,  
+ * and an ID of 0 is not considered valid.  
+ */
+delete from alias_argument where id_alias = 0;
