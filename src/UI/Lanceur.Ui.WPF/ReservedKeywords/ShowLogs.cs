521834ae876d3683c21ba05284010e470d15620e
jibedoubleve <jibedoubleve@users.noreply.github.com>
2025-03-02 12:17:16 +0100

    (maint) Add keywords logs

---
+++ src/UI/Lanceur.Ui.WPF/ReservedKeywords/ShowLogs.cs
@@ -0,0 +1,40 @@
+using System.ComponentModel;
+using System.Diagnostics;
+using Lanceur.Core;
+using Lanceur.Core.Models;
+using Lanceur.Infra.Constants;
+
+namespace Lanceur.Ui.WPF.ReservedKeywords;
+
+[ReservedAlias("logs")]
+[Description("Open the directory containing the log files")]
+public class ShowLogs : SelfExecutableQueryResult
+{
+    #region Fields
+
+    private readonly IServiceProvider _serviceProvider;
+
+    #endregion
+
+    #region Constructors
+
+    public ShowLogs(IServiceProvider serviceProvider) => _serviceProvider = serviceProvider;
+
+    #endregion
+
+    #region Properties
+
+    public override string Icon => "Book24";
+
+    #endregion
+
+    #region Methods
+
+    public override Task<IEnumerable<QueryResult>> ExecuteAsync(Cmdline? cmdline = null)
+    {
+        Process.Start("explorer.exe", Paths.LogRepository);
+        return NoResultAsync;
+    }
+
+    #endregion
+}
