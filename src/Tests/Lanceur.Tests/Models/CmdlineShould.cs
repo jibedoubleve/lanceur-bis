cf931a37a265a3c93e8b70af9fe062661af2aad0
jibedoubleve <jibedoubleve@users.noreply.github.com>
2025-03-03 16:09:14 +0100

    (maint) Remove not use methods

--- src/Tests/Lanceur.Tests/Models/CmdlineShould.cs
+++ src/Tests/Lanceur.Tests/Models/CmdlineShould.cs
@@ -37,44 +37,6 @@ public class CmdlineShould
     }
 
     [Theory]
-    [InlineData("init", "un deux trois", "quatre cinq six")]
-    [InlineData("move", "un", "quatre cinq six")]
-    [InlineData("move", "", "quatre cinq six")]
-    [InlineData("?", "", "quatre cinq six")]
-    [InlineData("?", "un deux trois", "quatre cinq six")]
-    public void CloneCmdline(string name, string parameters, string newParameters)
-    {
-        // Arrange
-        var cmdline = $"{name} {parameters}";
-        var cmd = Cmdline.Parse(cmdline);
-
-        // Act
-        var newCmd = Cmdline.CloneWithNewParameters(newParameters, cmd);
-
-        // Assert
-        newCmd.Name.Should().Be(name);
-        newCmd.Parameters.Should().Be(newParameters);
-    }
-
-    [Theory]
-    [InlineData("un deux trois")]
-    [InlineData("quatre cinq six")]
-    public void CloneCmdlineWithEmptyParameters(string newParameters)
-    {
-        // Arrange
-        const string name = "init";
-        const string parameters = "un deux trois";
-        var cmd = Cmdline.Parse($"{name} {parameters}");
-
-        // Act
-        var newCmd = Cmdline.CloneWithNewParameters(newParameters, cmd);
-
-        // Assert
-        newCmd.Name.Should().Be(name);
-        newCmd.Parameters.Should().Be(newParameters);
-    }
-
-    [Theory]
     [InlineData("$AAAA aa")]
     [InlineData("&AAAA aa")]
     [InlineData("|AAAA aa")]
