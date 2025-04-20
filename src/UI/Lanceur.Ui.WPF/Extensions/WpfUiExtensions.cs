135feeb575761cd154b1f59394a334a0dfdfe7d3
jibedoubleve <jibedoubleve@users.noreply.github.com>
2025-04-20 21:52:26 +0200

    (maint) Refactoring

--- src/UI/Lanceur.Ui.WPF/Extensions/WpfUiExtensions.cs
+++ src/UI/Lanceur.Ui.WPF/Extensions/WpfUiExtensions.cs
@@ -9,11 +9,11 @@ public static class WpfUiExtensions
 
     public static WindowBackdropType ToWindowBackdropType(this string value)
     {
-        if (string.Equals(value, WindowBackdropType.Acrylic.ToString(), StringComparison.CurrentCultureIgnoreCase)) return WindowBackdropType.Acrylic;
-        if (string.Equals(value, WindowBackdropType.None.ToString(), StringComparison.CurrentCultureIgnoreCase)) return WindowBackdropType.None;
-        if (string.Equals(value, WindowBackdropType.Auto.ToString(), StringComparison.CurrentCultureIgnoreCase)) return WindowBackdropType.Auto;
-        if (string.Equals(value, WindowBackdropType.Mica.ToString(), StringComparison.CurrentCultureIgnoreCase)) return WindowBackdropType.Mica;
-        if (string.Equals(value, WindowBackdropType.Tabbed.ToString(), StringComparison.CurrentCultureIgnoreCase)) return WindowBackdropType.Tabbed;
+        if (string.Equals(value, nameof(WindowBackdropType.Acrylic), StringComparison.CurrentCultureIgnoreCase)) return WindowBackdropType.Acrylic;
+        if (string.Equals(value, nameof(WindowBackdropType.None), StringComparison.CurrentCultureIgnoreCase)) return WindowBackdropType.None;
+        if (string.Equals(value, nameof(WindowBackdropType.Auto), StringComparison.CurrentCultureIgnoreCase)) return WindowBackdropType.Auto;
+        if (string.Equals(value, nameof(WindowBackdropType.Mica), StringComparison.CurrentCultureIgnoreCase)) return WindowBackdropType.Mica;
+        if (string.Equals(value, nameof(WindowBackdropType.Tabbed), StringComparison.CurrentCultureIgnoreCase)) return WindowBackdropType.Tabbed;
 
         return ConditionalExecution.Execute(
             () => throw new NotSupportedException($"{value} is not a wpf backdrop type."),
