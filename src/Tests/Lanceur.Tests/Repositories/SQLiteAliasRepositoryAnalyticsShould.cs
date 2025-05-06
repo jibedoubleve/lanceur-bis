80c6f58da9e438cd4e721516086d8b2caa2a112a
jibedoubleve <jibedoubleve@users.noreply.github.com>
2025-05-06 19:45:38 +0200

    (#960) Tests

--- src/Tests/Lanceur.Tests/Repositories/SQLiteAliasRepositoryAnalyticsShould.cs
+++ src/Tests/Lanceur.Tests/Repositories/SQLiteAliasRepositoryAnalyticsShould.cs
@@ -1,12 +1,14 @@
+using FluentAssertions;
+using FluentAssertions.Execution;
 using Lanceur.Core.Mappers;
 using Lanceur.Core.Models;
 using Lanceur.Core.Repositories;
-using Lanceur.Core.Services;
 using Lanceur.Core.Utils;
 using Lanceur.Infra.SQLite.DataAccess;
 using Lanceur.Infra.SQLite.DbActions;
 using Lanceur.Infra.SQLite.Repositories;
 using Lanceur.Tests.Tools;
+using Lanceur.Tests.Tools.SQL;
 using Microsoft.Extensions.Logging;
 using NSubstitute;
 using Xunit;
@@ -29,45 +31,438 @@ public class SQLiteAliasRepositoryQueryShouldBeValid : TestBase
 
     #region Methods
 
-    private SQLiteAliasRepository BuildRepository(IConnectionString connectionString = null)
+    private SQLiteAliasRepository BuildRepository(string sql = null, IConnectionString connectionString = null)
     {
-        var connection = BuildFreshDb(connectionString: connectionString);
+        var connection = BuildFreshDb(sql, connectionString);
         var scope = new DbSingleConnectionManager(connection);
         var log = Substitute.For<ILoggerFactory>();
-        var conv = Substitute.For<IMappingService>();
+        var mappingService = new MappingService();
         var service = new SQLiteAliasRepository(
             scope,
             log,
-            conv,
-            new DbActionFactory(new MappingService(), log)
+            mappingService,
+            new DbActionFactory(mappingService, log)
         );
         return service;
     }
 
     [Fact] public void GetAdditionalParameter() => BuildRepository().GetAdditionalParameter([1, 2, 3]);
-    
-    [Fact] public void GetAliasesWithoutNotes() => BuildRepository().GetAliasesWithoutNotes();
-    
+
+    [Fact]
+    public void GetAliasesWithoutNotes()
+    {
+        // arrange
+        var i = 0;
+        var date1 = DateTime.Now.AddDays(-1);
+        var date2 = DateTime.Now.AddDays(-2);
+
+        var sql = new SqlBuilder().AppendAlias(
+                                      ++i,
+                                      cfg: a =>
+                                      {
+                                          a.WithUsage(date1, date2);
+                                          a.WithSynonyms();
+                                      }
+                                  )
+                                  .AppendAlias(
+                                      ++i,
+                                      cfg: a =>
+                                      {
+                                          a.WithUsage(date1, date2);
+                                          a.WithSynonyms();
+                                      }
+                                  )
+                                  .AppendAlias(
+                                      ++i,
+                                      cfg: a =>
+                                      {
+                                          a.WithUsage(date1, date2);
+                                          a.WithSynonyms();
+                                      }
+                                  )
+                                  .AppendAlias(
+                                      ++i,
+                                      cfg: a =>
+                                      {
+                                          a.WithUsage(date1, date2);
+                                          a.WithSynonyms();
+                                      }
+                                  )
+                                  .AppendAlias(
+                                      ++i,
+                                      cfg: a =>
+                                      {
+                                          a.WithUsage(date1, date2);
+                                          a.WithSynonyms();
+                                      }
+                                  )
+                                  .ToString();
+        var service = BuildRepository(sql);
+
+        // act
+        var aliases = service.GetAliasesWithoutNotes()
+                             .ToArray();
+
+        // assert
+        using (new AssertionScope())
+        {
+            aliases.Should().HaveCount(i);
+            foreach (var alias in aliases) alias.Count.Should().Be(2, $"alias {alias.Name} has usage");
+        }
+    }
+
     [Fact] public void GetAll() => BuildRepository().GetAll();
-    
+
     [Fact] public void GetAllAliasWithAdditionalParameters() => BuildRepository().GetAllAliasWithAdditionalParameters();
-    
-    [Fact] public void GetBrokenAliases() => BuildRepository().GetBrokenAliases();
-    
+
+    [Theory]
+    [InlineData(@"c:\Not\A\Real\File.exe")]
+    [InlineData(@"C:\NOT\A\REAL\FILE.EXE")]
+    [InlineData(@"c:\not\a\real\file\")]
+    [InlineData(@"C:\NOT\A\REAL\FILE\")]
+    public void GetBrokenAliases(string fileName)
+    {
+        // arrange
+        var i = 0;
+        var date1 = DateTime.Now.AddDays(-1);
+        var date2 = DateTime.Now.AddDays(-2);
+
+        var sql = new SqlBuilder().AppendAlias(
+                                      ++i,
+                                      $"{fileName}_{i}",
+                                      cfg: a =>
+                                      {
+                                          a.WithUsage(date1, date2);
+                                          a.WithSynonyms();
+                                      }
+                                  )
+                                  .AppendAlias(
+                                      ++i,
+                                      $"{fileName}_{i}",
+                                      cfg: a =>
+                                      {
+                                          a.WithUsage(date1, date2);
+                                          a.WithSynonyms();
+                                      }
+                                  )
+                                  .AppendAlias(
+                                      ++i,
+                                      $"{fileName}_{i}",
+                                      cfg: a =>
+                                      {
+                                          a.WithUsage(date1, date2);
+                                          a.WithSynonyms();
+                                      }
+                                  )
+                                  .AppendAlias(
+                                      ++i,
+                                      $"{fileName}_{i}",
+                                      cfg: a =>
+                                      {
+                                          a.WithUsage(date1, date2);
+                                          a.WithSynonyms();
+                                      }
+                                  )
+                                  .AppendAlias(
+                                      ++i,
+                                      $"{fileName}_{i}",
+                                      cfg: a =>
+                                      {
+                                          a.WithUsage(date1, date2);
+                                          a.WithSynonyms();
+                                      }
+                                  )
+                                  .ToString();
+        var service = BuildRepository(sql);
+
+        // act
+        var aliases = service.GetBrokenAliases()
+                             .ToArray();
+
+        // assert
+        using (new AssertionScope())
+        {
+            aliases.Should().HaveCount(i);
+            foreach (var alias in aliases) alias.Count.Should().Be(2, $"alias {alias.Name} has usage");
+        }
+    }
+
     [Fact] public void GetById() => BuildRepository().GetById(1);
-    
-    [Fact] public void GetDeletedAlias() => BuildRepository().GetDeletedAlias();
-    
-    [Fact] public void GetDoubloons() => BuildRepository().GetDoubloons();
-    
-    [Fact] public void GetExistingAliases() => BuildRepository().GetExistingAliases([], 0);
-    
+
+    [Fact]
+    public void GetDeletedAlias()
+    {
+        // arrange
+        var i = 0;
+        var now = DateTime.Now.AddMinutes(-60);
+        var date1 = now.AddDays(-1);
+        var date2 = now.AddDays(-2);
+
+        var sql = new SqlBuilder().AppendAlias(
+                                      ++i,
+                                      props: new(DeletedAt: now),
+                                      cfg: a =>
+                                      {
+                                          a.WithUsage(date1, date2);
+                                          a.WithSynonyms();
+                                      }
+                                  )
+                                  .AppendAlias(
+                                      ++i,
+                                      props: new(DeletedAt: now),
+                                      cfg: a =>
+                                      {
+                                          a.WithUsage(date1, date2);
+                                          a.WithSynonyms();
+                                      }
+                                  )
+                                  .AppendAlias(
+                                      ++i,
+                                      props: new(DeletedAt: now),
+                                      cfg: a =>
+                                      {
+                                          a.WithUsage(date1, date2);
+                                          a.WithSynonyms();
+                                      }
+                                  )
+                                  .AppendAlias(
+                                      ++i,
+                                      props: new(DeletedAt: now),
+                                      cfg: a =>
+                                      {
+                                          a.WithUsage(date1, date2);
+                                          a.WithSynonyms();
+                                      }
+                                  )
+                                  .AppendAlias(
+                                      ++i,
+                                      props: new(DeletedAt: now),
+                                      cfg: a =>
+                                      {
+                                          a.WithUsage(date1, date2);
+                                          a.WithSynonyms();
+                                      }
+                                  )
+                                  .ToString();
+        var service = BuildRepository(sql);
+
+        // act
+        var aliases = service.GetDeletedAlias()
+                             .ToArray();
+
+        // assert
+        using (new AssertionScope())
+        {
+            aliases.Should().HaveCount(i);
+            foreach (var alias in aliases) alias.Count.Should().Be(2, $"alias {alias.Name} has usage");
+        }
+    }
+
+    [Fact]
+    public void GetDoubloons()
+    {
+        // arrange
+        var i = 0;
+        const string name = "is_a_doubloon";
+        var sql = new SqlBuilder().AppendAlias(++i, name, name)
+                                  .AppendAlias(++i, name, name)
+                                  .AppendAlias(++i, name, name)
+                                  .AppendAlias(++i, name, name)
+                                  .AppendAlias(++i, name, name)
+                                  .ToString();
+        var service = BuildRepository(sql);
+
+        // act
+        var aliases = service.GetDoubloons()
+                             .ToArray();
+
+        // assert
+        using (new AssertionScope())
+        {
+            aliases.Should().HaveCount(i);
+            foreach (var alias in aliases) alias.Count.Should().Be(2, $"alias {alias.Name} has usage");
+        }
+    }
+
+    [Fact]
+    public void GetExistingAliases()
+    {
+        // arrange
+        var i = 0;
+        var sql = new SqlBuilder().AppendAlias(++i, cfg: a => a.WithSynonyms("name_1"))
+                                  .AppendAlias(++i, cfg: a => a.WithSynonyms("name_1"))
+                                  .AppendAlias(++i, cfg: a => a.WithSynonyms("name_1"))
+                                  .AppendAlias(++i, cfg: a => a.WithSynonyms("name_1"))
+                                  .AppendAlias(++i, cfg: a => a.WithSynonyms("name_1"))
+                                  .ToString();
+        var service = BuildRepository(sql);
+
+        // act
+        const string name = "name_1";
+        var aliases = service.GetExistingAliases([name, "no_exist", "still_no_exist"], 1)
+                             .ToArray();
+
+        // assert
+        using (new AssertionScope())
+        {
+            aliases.Should().HaveCount(4);
+            aliases[0].Should().Be(name);
+        }
+    }
+
     [Fact] public void GetExistingDeletedAliases() => BuildRepository().GetExistingDeletedAliases([], 0);
-    
+
     [Fact] public void GetHiddenCounters() => BuildRepository().GetHiddenCounters();
-    
+
+
+    [Fact]
+    public void GetInactiveAliases()
+    {
+        // arrange
+        var i = 0;
+        var date1 = DateTime.Now.AddMonths(-10);
+        var date2 = DateTime.Now.AddMonths(-20);
+        var sql = new SqlBuilder().AppendAlias(
+                                      ++i,
+                                      cfg: a =>
+                                      {
+                                          a.WithSynonyms();
+                                          a.WithUsage(date1, date2);
+                                      }
+                                  )
+                                  .AppendAlias(
+                                      ++i,
+                                      cfg: a =>
+                                      {
+                                          a.WithSynonyms();
+                                          a.WithUsage(date1, date2);
+                                      }
+                                  )
+                                  .AppendAlias(
+                                      ++i,
+                                      cfg: a =>
+                                      {
+                                          a.WithSynonyms();
+                                          a.WithUsage(date1, date2);
+                                      }
+                                  )
+                                  .AppendAlias(
+                                      ++i,
+                                      cfg: a =>
+                                      {
+                                          a.WithSynonyms();
+                                          a.WithUsage(date1, date2);
+                                      }
+                                  )
+                                  .AppendAlias(
+                                      ++i,
+                                      cfg: a =>
+                                      {
+                                          a.WithSynonyms();
+                                          a.WithUsage(date1, date2);
+                                      }
+                                  )
+                                  .ToString();
+        var service = BuildRepository(sql);
+
+        // act
+        var aliases = service.GetInactiveAliases(1)
+                             .ToArray();
+
+        // assert
+        using (new AssertionScope())
+        {
+            aliases.Should().HaveCount(4);
+        }
+    }
+    [Fact]
+    public void GetRarelyUsedAliases()
+    {
+        // arrange
+        var i = 0;
+        var date1 = DateTime.Now.AddMonths(-10);
+        var date2 = DateTime.Now.AddMonths(-20);
+        var sql = new SqlBuilder().AppendAlias(
+                                      ++i,
+                                      cfg: a =>
+                                      {
+                                          a.WithSynonyms();
+                                          a.WithUsage(date1, date2);
+                                      }
+                                  )
+                                  .AppendAlias(
+                                      ++i,
+                                      cfg: a =>
+                                      {
+                                          a.WithSynonyms();
+                                          a.WithUsage(date1, date2);
+                                      }
+                                  )
+                                  .AppendAlias(
+                                      ++i,
+                                      cfg: a =>
+                                      {
+                                          a.WithSynonyms();
+                                          a.WithUsage(date1, date2);
+                                      }
+                                  )
+                                  .AppendAlias(
+                                      ++i,
+                                      cfg: a =>
+                                      {
+                                          a.WithSynonyms();
+                                          a.WithUsage(date1, date2);
+                                      }
+                                  )
+                                  .AppendAlias(
+                                      ++i,
+                                      cfg: a =>
+                                      {
+                                          a.WithSynonyms();
+                                          a.WithUsage(date1, date2);
+                                      }
+                                  )
+                                  .ToString();
+        var service = BuildRepository(sql);
+
+        // act
+        var aliases = service.GetRarelyUsedAliases(5)
+                             .ToArray();
+
+        // assert
+        using (new AssertionScope())
+        {
+            aliases.Should().HaveCount(4);
+        }
+    }
+
+    [Fact]
+    public void GetUnusedAliases()
+    {
+        // arrange
+        var i = 0;
+        var sql = new SqlBuilder().AppendAlias(++i, cfg: a => a.WithSynonyms())
+                                  .AppendAlias(++i, cfg: a => a.WithSynonyms())
+                                  .AppendAlias(++i, cfg: a =>  a.WithSynonyms())
+                                  .AppendAlias(++i, cfg: a => a.WithSynonyms())
+                                  .AppendAlias(++i, cfg: a => a.WithSynonyms())
+                                  .ToString();
+        var service = BuildRepository(sql);
+
+        // act
+        var aliases = service.GetUnusedAliases()
+                             .ToArray();
+
+        // assert
+        using (new AssertionScope())
+        {
+            aliases.Should().HaveCount(i);
+            foreach (var alias in aliases) alias.Count.Should().Be(0);
+        }
+    }
+
     [Fact] public void GetUsage() => BuildRepository().GetUsage(Per.DayOfWeek);
-    
+
     [Fact] public void GetYearsWithUsage() => BuildRepository().GetYearsWithUsage();
 
     [Fact] public void HydrateAlias() => BuildRepository().HydrateAlias(new());
