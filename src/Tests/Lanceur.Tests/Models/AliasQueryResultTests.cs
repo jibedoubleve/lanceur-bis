9766360e858c300407731b32cb368a2dee4be91c
jibedoubleve <jibedoubleve@users.noreply.github.com>
2026-02-19 16:28:47 +0100

    (#1175) Add regression tests

---
+++ src/Tests/Lanceur.Tests/Models/AliasQueryResultTests.cs
@@ -0,0 +1,32 @@
+using Lanceur.Core.BusinessLogic;
+using Lanceur.Core.Models;
+using Shouldly;
+using Xunit;
+
+namespace Lanceur.Tests.Models;
+
+public class AliasQueryResultTests
+{
+    #region Methods
+
+    /// <summary>
+    ///     Regression test for issue #1175. Check the sanitisation can be run against null.
+    /// </summary>
+    [Theory]
+    [InlineData(null, null)]
+    [InlineData(@"C:\dir\dir2\file.exe", @"C:\dir\dir2\file.exe")]
+    [InlineData(@"""C:\dir\dir2\file.exe""", @"C:\dir\dir2\file.exe")]
+    public void Alias_can_be_sanitized(string filename, string expected)
+    {
+        // Arrange
+        var alias = new AliasQueryResult { FileName = filename };
+
+        // Act
+        alias.SanitizeFileName();
+
+        // Assert
+        alias.FileName.ShouldBe(expected);
+    }
+
+    #endregion
+}
