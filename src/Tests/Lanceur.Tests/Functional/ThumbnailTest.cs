9f77fd37e5c7ba3f2c248bdd9fbb9790197820ff
jibedoubleve <jibedoubleve@users.noreply.github.com>
2026-02-27 11:03:16 +0100

    (#1224) Rename tests

--- src/Tests/Lanceur.Tests/Functional/ThumbnailTest.cs
+++ src/Tests/Lanceur.Tests/Functional/ThumbnailTest.cs
@@ -1,23 +1,23 @@
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using Lanceur.Infra.Win32.Images;
+using Lanceur.Infra.Win32.Extensions;
 using Shouldly;
 using Xunit;
 
 namespace Lanceur.Tests.Functional;
 
-public class ThumbnailShould
+public class ThumbnailTest
 {
     #region Methods
 
-    private static IEnumerable<object> NotFailWhenCopyingImageSourceThumbnailSource()
+    private static IEnumerable<object> SourceDestinationBitmapSource()
     {
         yield return new object[] { null, Path.GetTempPath() };
         yield return new object[] { new BitmapImage(), null };
         yield return new object[] { null, null };
     }
 
-    private static IEnumerable<object> NotFailWhenCopyingThumbnailSource()
+    private static IEnumerable<object> SourceDestinationPathSource()
     {
         yield return new object[] { null, Path.GetTempPath() };
         yield return new object[] { Path.GetTempFileName(), null };
@@ -26,7 +26,7 @@ public class ThumbnailShould
 
     [Theory]
     [InlineData((string)null)]
-    public void NotFailsWhenCopyThumbnailToImageSourceRepository(string destinationPath)
+    public void When_copy_thumbnail_to_ImageSourceRepository_Then_no_error(string destinationPath)
     {
         var thumbnailPath = new BitmapImage();
         var action = () => thumbnailPath.CopyToImageRepository(destinationPath);
@@ -34,16 +34,16 @@ public class ThumbnailShould
     }
 
     [Theory]
-    [MemberData(nameof(NotFailWhenCopyingThumbnailSource))]
-    public void NotFailsWhenCopyThumbnailToRepository(string thumbnailPath, string destinationPath)
+    [MemberData(nameof(SourceDestinationPathSource))]
+    public void When_copy_thumbnail_to_repository_Then_no_error(string thumbnailPath, string destinationPath)
     {
         var action = () => thumbnailPath.CopyToImageRepository(destinationPath);
         action.ShouldNotThrow();
     }
 
     [StaTheory]
-    [MemberData(nameof(NotFailWhenCopyingImageSourceThumbnailSource))]
-    public void NotFailWhenCopyingImageSourceThumbnail(ImageSource thumbnail, string destinationPath)
+    [MemberData(nameof(SourceDestinationBitmapSource))]
+    public void When_copying_ImageSourceThumbnail_Then_no_error(ImageSource thumbnail, string destinationPath)
     {
         var action = () => thumbnail.CopyToImageRepository(destinationPath);
         action.ShouldNotThrow();
