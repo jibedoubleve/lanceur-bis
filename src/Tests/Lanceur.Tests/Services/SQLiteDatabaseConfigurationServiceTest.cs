1a7cad047eb6a3083a0d11729613effdeb1355a1
jibedoubleve <jibedoubleve@users.noreply.github.com>
2026-03-04 13:50:26 +0100

    (#1224) Rename tests

--- src/Tests/Lanceur.Tests/Services/SQLiteDatabaseConfigurationServiceTest.cs
+++ src/Tests/Lanceur.Tests/Services/SQLiteDatabaseConfigurationServiceTest.cs
@@ -7,6 +7,7 @@ using Lanceur.Infra.Repositories;
 using Lanceur.Infra.SQLite.DataAccess;
 using Lanceur.Infra.SQLite.Repositories;
 using Lanceur.Tests.Tools;
+using Lanceur.Tests.Tools.Generators;
 using Newtonsoft.Json;
 using Shouldly;
 using Xunit;
@@ -14,22 +15,23 @@ using Version = System.Version;
 
 namespace Lanceur.Tests.Services;
 
-public class SQLiteDatabaseConfigurationServiceShould : TestBase
+public class SQLiteDatabaseConfigurationServiceTest : TestBase
 {
     #region Constructors
 
-    public SQLiteDatabaseConfigurationServiceShould(ITestOutputHelper output) : base(output) { }
+    public SQLiteDatabaseConfigurationServiceTest(ITestOutputHelper output) : base(output) { }
 
     #endregion
 
     #region Methods
 
-    private void WithConfiguration(Action<IApplicationSettingsProvider> assert, string json = null, IConnectionString connectionString = null)
+    private void WithConfiguration(
+        Action<IApplicationSettingsProvider> assert, string json = null, IConnectionString connectionString = null)
     {
         var sql = $"insert into settings (s_key, s_value) values ('json', '{json}');";
-        
+
         connectionString ??= ConnectionStringFactory.InMemory;
-        
+
         using var c = BuildFreshDb(connectionString: connectionString);
         c.Execute(sql);
         using var scope = new DbSingleConnectionManager(c);
@@ -57,8 +59,70 @@ public class SQLiteDatabaseConfigurationServiceShould : TestBase
         assert(settingRepository.Current);
     }
 
+    public static IEnumerable<object[]> HaveUpdatedPropertyFeed()
+    {
+        yield return
+        [
+            new Action<ApplicationSettings>(cfg => cfg.Caching.StoreCacheDuration = 99),
+            new Action<ApplicationSettings>(cfg => cfg.Caching.StoreCacheDuration.ShouldBe(99))
+        ];
+        yield return
+        [
+            new Action<ApplicationSettings>(cfg => cfg.Caching.ThumbnailCacheDuration = 99),
+            new Action<ApplicationSettings>(cfg => cfg.Caching.ThumbnailCacheDuration.ShouldBe(99))
+        ];
+        yield return
+        [
+            new Action<ApplicationSettings>(cfg => cfg.Github.Tag = "hello world"),
+            new Action<ApplicationSettings>(cfg => cfg.Github.Tag.ShouldBe("hello world"))
+        ];
+        yield return
+        [
+            new Action<ApplicationSettings>(_ => { }),
+            new Action<ApplicationSettings>(cfg => cfg.Github.Tag.ShouldBe("ungroomed"))
+        ];
+    }
+
     [Fact]
-    public void CreateFileWhenNotExists()
+    public void When_deserialising_feature_flags_Then_obsoletes_are_deleted()
+    {
+        var features = Features.GetNames();
+        var json = $$"""
+                     {
+                         "FeatureFlags": [
+                             {
+                                 "Description": "{{Generate.Text()}}",
+                                 "Enabled": false,
+                                 "FeatureName": "{{Generate.Text()}}",
+                                 "Icon": "{{Generate.Text()}}"
+                             },
+                             {
+                                 "Description": "{{Generate.Text()}}",
+                                 "Enabled": true,
+                                 "FeatureName": "{{Generate.Text()}}",
+                                 "Icon": "{{Generate.Text()}}"
+                             },
+                             {
+                                 "Description": "{{Generate.Text()}}",
+                                 "Enabled": true,
+                                 "FeatureName": "{{Generate.Text()}}",
+                                 "Icon": "{{Generate.Text()}}"
+                             }
+                         ]
+                     }
+                     """;
+        WithConfiguration(repository =>
+                repository.Current.FeatureFlags.ToList()
+                          .ShouldSatisfyAllConditions(
+                              ff => ff.Count.ShouldBeGreaterThan(0),
+                              ff => ff.Select(f => features.Contains(f.FeatureName))
+                                      .Count().ShouldBe(features.Count())
+                          ),
+            json);
+    }
+
+    [Fact]
+    public void When_no_db_Then_a_fresh_one_is_created()
     {
         var file = Path.GetTempFileName();
         var stg = new JsonInfrastructureSettingsProvider(file);
@@ -70,12 +134,51 @@ public class SQLiteDatabaseConfigurationServiceShould : TestBase
     }
 
     [Fact]
-    public void DeserialiseFeatureFlags()
+    public void When_retrieving_default_settings_Then_default_position_has_the_expected_value() =>
+        WithConfiguration(repository => {
+                var settings = repository.Current;
+                settings.Window.Position.Left.ShouldBe(double.MaxValue);
+                settings.Window.Position.Top.ShouldBe(double.MaxValue);
+            }
+        );
+
+    [Fact]
+    public void When_retrieving_default_settings_Then_HotKey_has_the_expected_value() =>
+        WithConfiguration(repository => {
+                var settings = repository.Current;
+                settings.HotKey.ModifierKey.ShouldBe(3);
+                settings.HotKey.Key.ShouldBe(18);
+            }
+        );
+
+    [Fact]
+    public void When_retrieving_default_settings_Then_ShowAtStartup_is_set_to_true()
     {
-        var description = Tools.Generators.Generate.Text();
+        var c = BuildFreshDb();
+        using var scope = new DbSingleConnectionManager(c);
+        var settings = new SQLiteApplicationSettingsProvider(scope, CreateLogger<SQLiteApplicationSettingsProvider>());
+
+        settings.Current.SearchBox.ShowAtStartup.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void When_retrieving_default_settings_Then_ShowResult_is_set_to_true()
+    {
+        var c = BuildFreshDb();
+        using var scope = new DbSingleConnectionManager(c);
+        var logger = CreateLogger<SQLiteApplicationSettingsProvider>();
+        var settings = new SQLiteApplicationSettingsProvider(scope, logger);
+
+        settings.Current.SearchBox.ShowResult.ShouldBeFalse();
+    }
+
+    [Fact]
+    public void When_updating_feature_flas_Then_json_is_updated()
+    {
+        var description = Generate.Text();
         var featureName = Features.GetNames().ElementAt(0);
-        var icon = Tools.Generators.Generate.Text();
-        
+        var icon = Generate.Text();
+
         var json = $$"""
                      {
                          "FeatureFlags": [
@@ -106,8 +209,53 @@ public class SQLiteDatabaseConfigurationServiceShould : TestBase
             json);
     }
 
+
+    [Theory]
+    [InlineData(1, 2)]
+    [InlineData(10, 20)]
+    public void When_updating_HotKey_Then_it_is_saved_in_db(int modifierKey, int key) =>
+        WithConfiguration(repository => {
+                var settings = repository.Current;
+                settings.SetHotKey(key, modifierKey);
+
+                repository.Save();
+                settings.HotKey.ModifierKey.ShouldBe(modifierKey);
+                settings.HotKey.Key.ShouldBe(key);
+            }
+        );
+
     [Fact]
-    public void GetAndSetData()
+    public void When_updating_json_Then_it_is_saved_in_db()
+    {
+        var file = Path.GetTempFileName();
+        var stg = new JsonInfrastructureSettingsProvider(file) { Current = { DbPath = "un_deux_trois" } };
+
+        stg.Save();
+
+        var asThisJson = JsonConvert.SerializeObject(stg.Current);
+        var json = File.ReadAllText(file);
+
+        json.ShouldBe(asThisJson);
+    }
+
+    [Theory]
+    [InlineData(1.1d, 2.1d)]
+    public void When_updating_position_Then_it_is_saved_in_db(double left, double top) =>
+        WithConfiguration(repository => {
+                var settings = repository.Current;
+                settings.Window.Position.Left = left;
+                settings.Window.Position.Top = top;
+
+                repository.Save();
+
+                var loaded = repository.Current;
+                loaded.Window.Position.Left.ShouldBe(left);
+                loaded.Window.Position.Top.ShouldBe(top);
+            }
+        );
+
+    [Fact]
+    public void When_updating_settings_Then_values_are_updated_when_retrieving()
     {
         var file = Path.GetTempFileName();
         var stg = new JsonInfrastructureSettingsProvider(file);
@@ -121,13 +269,19 @@ public class SQLiteDatabaseConfigurationServiceShould : TestBase
         actual.ShouldBe(expected);
     }
 
+    [Theory]
+    [MemberData(nameof(HaveUpdatedPropertyFeed))]
+    public void When_updating_value_Then_it_is_saved_in_db(
+        Action<ApplicationSettings> update, Action<ApplicationSettings> assert)
+        => WithConfiguration(update, assert);
+
     [Fact]
-    public void HandleAllProperties()
+    public void When_updating_values_in_settings_Then_retrieving_updated_values()
     {
-        var features = Features.GetNames().ToList(); 
-        var description = Tools.Generators.Generate.Text();
-        var icon = Tools.Generators.Generate.Text();
-        
+        var features = Features.GetNames().ToList();
+        var description = Generate.Text();
+        var icon = Generate.Text();
+
         var json = $$"""
                      {
                          "Caching": {
@@ -204,28 +358,21 @@ public class SQLiteDatabaseConfigurationServiceShould : TestBase
                         }
                         else { Assert.Fail("No feature flags"); }
                     },
-
                     a => a.Github.LastCheckedVersion.ShouldBe(new Version("1.2.3")),
                     a => a.Github.SnoozeVersionCheck.ShouldBe(true),
                     a => a.Github.Token.ShouldBe("123456789"),
-
                     a => a.HotKey.Key.ShouldBe(55),
                     a => a.HotKey.ModifierKey.ShouldBe(56),
-
                     a => a.Reconciliation.InactivityThreshold.ShouldBe(99),
                     a => a.Reconciliation.LowUsageThreshold.ShouldBe(98),
-
                     a => a.ResourceMonitor.CpuSmoothingIndex.ShouldBe(66),
                     a => a.ResourceMonitor.RefreshRate.ShouldBe(77),
-
                     a => a.SearchBox.SearchDelay.ShouldBe(11.99),
                     a => a.SearchBox.ShowAtStartup.ShouldBe(false),
                     a => a.SearchBox.ShowLastQuery.ShouldBe(false),
                     a => a.SearchBox.ShowResult.ShouldBe(true),
-
                     a => a.Stores.BookmarkSourceBrowser.ShouldBe("Edge"),
                     a => a.Stores.EverythingQuerySuffix.ShouldBe("eee"),
-
                     a => {
                         if (a.Stores.StoreShortcuts.Count() == 2)
                         {
@@ -237,163 +384,11 @@ public class SQLiteDatabaseConfigurationServiceShould : TestBase
                         }
                         else { Assert.Fail("No store StoreShortcuts"); }
                     },
-
                     a => a.Window.BackdropStyle.ShouldBe("Micass"),
                     a => a.Window.NotificationDisplayDuration.ShouldBe(11),
                     a => a.Window.Position.Left.ShouldBe(1.23456),
                     a => a.Window.Position.Top.ShouldBe(2.45678)
                 ),
-            json);
-    }
-
-    [Fact]
-    public void HaveDefaultHotKey() =>
-        WithConfiguration(repository => {
-                var settings = repository.Current;
-                settings.HotKey.ModifierKey.ShouldBe(3);
-                settings.HotKey.Key.ShouldBe(18);
-            }
-        );
-
-    [Fact]
-    public void HaveDefaultPosition() =>
-        WithConfiguration(repository => {
-                var settings = repository.Current;
-                settings.Window.Position.Left.ShouldBe(double.MaxValue);
-                settings.Window.Position.Top.ShouldBe(double.MaxValue);
-            }
-        );
-
-    [Fact]
-    public void HaveDefaultShowAtStartup()
-    {
-        var c = BuildFreshDb();
-        using var scope = new DbSingleConnectionManager(c);
-        var settings = new SQLiteApplicationSettingsProvider(scope, CreateLogger<SQLiteApplicationSettingsProvider>());
-
-        settings.Current.SearchBox.ShowAtStartup.ShouldBeTrue();
-    }
-
-    [Fact]
-    public void HaveDefaultShowResult()
-    {
-        var c = BuildFreshDb();
-        using var scope = new DbSingleConnectionManager(c);
-        var logger = CreateLogger<SQLiteApplicationSettingsProvider>();
-        var settings = new SQLiteApplicationSettingsProvider(scope, logger);
-
-        settings.Current.SearchBox.ShowResult.ShouldBeFalse();
-    }
-
-    [Theory]
-    [MemberData(nameof(HaveUpdatedPropertyFeed))]
-    public void HaveUpdatedProperty(Action<ApplicationSettings> update, Action<ApplicationSettings> assert)
-        => WithConfiguration(update, assert);
-
-    public static IEnumerable<object[]> HaveUpdatedPropertyFeed()
-    {
-        yield return
-        [
-            new Action<ApplicationSettings>(cfg => cfg.Caching.StoreCacheDuration = 99),
-            new Action<ApplicationSettings>(cfg => cfg.Caching.StoreCacheDuration.ShouldBe(99))
-        ];
-        yield return
-        [
-            new Action<ApplicationSettings>(cfg => cfg.Caching.ThumbnailCacheDuration = 99),
-            new Action<ApplicationSettings>(cfg => cfg.Caching.ThumbnailCacheDuration.ShouldBe(99))
-        ];
-        yield return
-        [
-            new Action<ApplicationSettings>(cfg => cfg.Github.Tag = "hello world"),
-            new Action<ApplicationSettings>(cfg => cfg.Github.Tag.ShouldBe("hello world"))
-        ];
-        yield return
-        [
-            new Action<ApplicationSettings>(_ => { }),
-            new Action<ApplicationSettings>(cfg => cfg.Github.Tag.ShouldBe("ungroomed"))
-        ];
-    }
-
-
-    [Theory]
-    [InlineData(1, 2)]
-    [InlineData(10, 20)]
-    public void SaveHotKey(int modifierKey, int key) =>
-        WithConfiguration(repository => {
-                var settings = repository.Current;
-                settings.SetHotKey(key, modifierKey);
-
-                repository.Save();
-                settings.HotKey.ModifierKey.ShouldBe(modifierKey);
-                settings.HotKey.Key.ShouldBe(key);
-            }
-        );
-
-    [Fact]
-    public void SaveJsonData()
-    {
-        var file = Path.GetTempFileName();
-        var stg = new JsonInfrastructureSettingsProvider(file) { Current = { DbPath = "un_deux_trois" } };
-
-        stg.Save();
-
-        var asThisJson = JsonConvert.SerializeObject(stg.Current);
-        var json = File.ReadAllText(file);
-
-        json.ShouldBe(asThisJson);
-    }
-
-    [Theory]
-    [InlineData(1.1d, 2.1d)]
-    public void SavePosition(double left, double top) =>
-        WithConfiguration(repository => {
-                var settings = repository.Current;
-                settings.Window.Position.Left = left;
-                settings.Window.Position.Top = top;
-
-                repository.Save();
-
-                var loaded = repository.Current;
-                loaded.Window.Position.Left.ShouldBe(left);
-                loaded.Window.Position.Top.ShouldBe(top);
-            }
-        );
-    
-    [Fact]
-    public void When_deserialising_feature_flags_Then_obsoletes_are_deleted()
-    {
-        var features = Features.GetNames();
-        var json = $$"""
-                     {
-                         "FeatureFlags": [
-                             {
-                                 "Description": "{{Tools.Generators.Generate.Text()}}",
-                                 "Enabled": false,
-                                 "FeatureName": "{{Tools.Generators.Generate.Text()}}",
-                                 "Icon": "{{Tools.Generators.Generate.Text()}}"
-                             },
-                             {
-                                 "Description": "{{Tools.Generators.Generate.Text()}}",
-                                 "Enabled": true,
-                                 "FeatureName": "{{Tools.Generators.Generate.Text()}}",
-                                 "Icon": "{{Tools.Generators.Generate.Text()}}"
-                             },
-                             {
-                                 "Description": "{{Tools.Generators.Generate.Text()}}",
-                                 "Enabled": true,
-                                 "FeatureName": "{{Tools.Generators.Generate.Text()}}",
-                                 "Icon": "{{Tools.Generators.Generate.Text()}}"
-                             }
-                         ]
-                     }
-                     """;
-        WithConfiguration(repository =>
-                repository.Current.FeatureFlags.ToList()
-                          .ShouldSatisfyAllConditions(
-                              ff => ff.Count.ShouldBeGreaterThan(0),
-                              ff => ff.Select(f=>features.Contains(f.FeatureName))
-                                      .Count().ShouldBe(features.Count())
-                          ),
             json);
     }
 
